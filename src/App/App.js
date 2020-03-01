62141b6c8d927ae10149f38fc572cc082bbe6ce1
Luis Angulo <builmiguel@gmail.com>
2020-03-01 20:33:19 +0000

    [FE-RT] feat: Add orders from end-point

--- src/App/App.js
+++ src/App/App.js
@@ -1,21 +1,30 @@
 import React from 'react';
+import orderApi from '../Api/orderApi';
 import CommentComponent from './CommentComponent';
 import AprovalComponent from './AprovalComponent';
 
+
 class App extends React.Component {
-    constructor(){
-        super();
+    
+    state = { orders: [] };
 
-        this.state = { title: null };
-    }
-
-    onSearchSubmit(term) {
-        console.log(term);
+    onSearchSubmit = async term => {
+        const response = await orderApi.get('/orders/', {
+                params: {
+                    key: term,
+                    page_size: 20,
+                    page: 3
+                }
+            }
+        );
+        
+        this.setState({ orders : response.data.items });
     }
 
     render(){
         return (
             <div>
+                <p>{this.state.orders.length}</p>
                 <AprovalComponent>
                     <CommentComponent onSubmit={this.onSearchSubmit} author="Luis" posttime="10:00 AM" body="some text for the comment"/>
                 </AprovalComponent>
